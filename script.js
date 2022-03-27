7fd53e9de9737192ea5407a3209f94b2bc359e67
Bruno <brunocosta.1991@hotmail.com>
2022-03-27 15:38:18 -0300

    logica olisão

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background')
+let position = 0;
 let isJumping = false;
 
 function handleKeyUp(event) {
@@ -11,7 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -36,12 +36,29 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randonTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
 
+    let leftInterval = setInterval(() => {
+
+        if (cactusPosition < - 60) {
+            clearInterval(leftInterval)
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randonTime);
+
 }
 
 createCactus();
-document.addEventListener('keyup', handleKeyUp);
+document.addEventListener('keypress', handleKeyUp);
