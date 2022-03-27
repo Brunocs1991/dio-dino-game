36ad7a11c2e75721e0a95e640908f6fea1b4b524
Bruno <brunocosta.1991@hotmail.com>
2022-03-27 15:12:48 -0300

    configurando pulo dino e adicionando assets

---
+++ script.js
@@ -0,0 +1,34 @@
+const dino = document.querySelector('.dino');
+let isJumping = false;
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump();
+        }
+    }
+}
+
+function jump() {
+    let position = 0;
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval)
+            let donwInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(donwInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+document.addEventListener('keyup', handleKeyUp)
